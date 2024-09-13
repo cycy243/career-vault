c8ff0209f0d6c6c8cae69ab0bab89c1c8831e292
Cyril Auquier <cyrilauquier@gmail.com>
2024-09-13 20:31:22 +0200

    - make table sortable

--- src/views/TrackingView.vue
+++ src/views/TrackingView.vue
@@ -4,11 +4,11 @@
     <table>
       <thead>
         <tr>
-          <td>Society name</td>
-          <td>Post's name</td>
-          <td>Application date</td>
-          <td>Application response date</td>
-          <td>Is accepted</td>
+          <td @click="sort('societyName')">Society name</td>
+          <td @click="sort('jobTitle')">Post's name</td>
+          <td @click="sort('sendDate')">Application date</td>
+          <td @click="sort('responseDate')">Application response date</td>
+          <td @click="sort('positiveReponse')">Is accepted</td>
         </tr>
       </thead>
       <tbody>
@@ -16,7 +16,9 @@
           <td>{{ application.societyName }}</td>
           <td>{{ application.jobTitle }}</td>
           <td>{{ application.sendDate.toLocaleDateString() }}</td>
-          <td>{{ application.responseDate?.toLocaleDateString() ?? 'No response yet' }}</td>
+          <td>
+            {{ application.responseDate?.toLocaleDateString() ?? 'No response yet' }}
+          </td>
           <td>{{ application.positiveReponse ?? '?' }}</td>
         </tr>
       </tbody>
@@ -27,6 +29,9 @@
 import JobApplication from '@/modules/model/jobApplication'
 import { ref } from 'vue'
 
+const sortColumn = ref<string>()
+
+const ascending = ref(false)
 const jobApplications = ref<Array<JobApplication>>([
   new JobApplication('Fake society1', 'Fake post1', new Date(), undefined, undefined),
   new JobApplication('Fake society2', 'Fake post2', new Date(), undefined, undefined),
@@ -34,6 +39,24 @@ const jobApplications = ref<Array<JobApplication>>([
   new JobApplication('Fake society4', 'Fake post4', new Date(), undefined, undefined),
   new JobApplication('Fake society5', 'Fake post5', new Date(), undefined, undefined)
 ])
+
+function sort(col: string) {
+  if (sortColumn.value === col) {
+    ascending.value = !ascending.value
+  } else {
+    ascending.value = false
+    sortColumn.value = col
+  }
+  const isAscending = ascending.value
+  jobApplications.value.sort((a, b) => {
+    if (a[col] > b[col]) {
+      return isAscending ? 1 : -1
+    } else if (a[col] < b[col]) {
+      return isAscending ? -1 : 1
+    }
+    return 0
+  })
+}
 </script>
 <style lang="css" scoped>
 table {
