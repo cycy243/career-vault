1d1cf0f98b263a536780b06e6f511b611341ebae
Cyril Auquier <cyrilauquier@gmail.com>
2024-09-17 18:33:01 +0200

    add a factory method

--- src/modules/model/jobApplication.ts
+++ src/modules/model/jobApplication.ts
@@ -5,6 +5,27 @@ export default class JobApplication {
     public sendDate: Date | undefined,
     public positiveReponse: Boolean | undefined,
     public responseDate: Date | undefined,
-    public applicationLink?: string
+    public applicationLink?: string,
+    public applicationId?: string
   ) {}
+
+  static createJobApplication(object: {
+    societyName: string
+    jobTitle: string
+    sendDate: Date | undefined
+    positiveReponse: Boolean | undefined
+    responseDate: Date | undefined
+    applicationLink?: string
+    applicationId?: string
+  }): JobApplication {
+    return new JobApplication(
+      object.societyName,
+      object.jobTitle,
+      object.sendDate,
+      object.positiveReponse,
+      object.responseDate,
+      object.applicationLink,
+      object.applicationId
+    )
+  }
 }
