c4a286acf34d3a02a20d9914dc121cab4a930676
Cyril Auquier <cyrilauquier@gmail.com>
2024-09-20 19:54:43 +0200

    add export to excel method

--- src/modules/services/files/iExportJobApplication.ts
+++ src/modules/services/files/iExportJobApplication.ts
@@ -2,4 +2,5 @@ import type JobApplication from '@/modules/model/jobApplication'
 
 export default interface IExportJobApplication {
   exportToPdf(jobApplications: Array<JobApplication>): Promise<boolean>
+  exportToExcel(jobApplications: Array<JobApplication>): Promise<boolean>
 }
