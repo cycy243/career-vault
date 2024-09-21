2687fe2a60884abc557399853cde6e59c3d286b3
Cyril Auquier <cyrilauquier@gmail.com>
2024-09-21 20:44:48 +0200

    add plugin to visualize chunks size

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    visualizer({ open: true, filename: 'bundle-visualization.html' }) as PluginOption
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('xlsx-js-style')) {
+            return 'xlsx-js-style'
+          }
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+        minifyInternalExports: true
+      },
+      treeshake: true
     }
   }
 })
