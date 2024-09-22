6b49a5944a356c6b24ef670aa07761d626dac4c3
Cyril Auquier <cyrilauquier@gmail.com>
2024-09-22 11:25:47 +0200

    test the user connection

---
+++ cypress/e2e/auth/connect.cy.ts
@@ -0,0 +1,27 @@
+// Ensure that the user exist in the database if it's not found
+
+describe('Connect a user', () => {
+  it('visits the connect url', () => {
+    cy.visit('/login')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+    cy.contains('h1', 'Register')
+  })
+
+  it('connect user', () => {
+    cy.visit('/login')
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+    cy.contains('h1', 'Register')
+    cy.get('input[name="email"]').type(Cypress.env('test_connect_user_login'))
+    cy.get('input[name="password"]').type(Cypress.env('test_connect_user_pwd'))
+    cy.get('form button[type=submit]').click()
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+
+    cy.contains('h1', 'Your tracking')
+  })
+})
