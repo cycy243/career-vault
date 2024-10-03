35173b092c7d66453e83703529629cc998d98427
Cyril Auquier <cyrilauquier@gmail.com>
2024-10-03 08:33:49 +0200

    add log for debug purposes

--- src/modules/repository/implementation/firebase/firebase.ts
+++ src/modules/repository/implementation/firebase/firebase.ts
@@ -16,6 +16,15 @@ const app = initializeApp({
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
 })
 
+console.log({
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCK,
+  appId: import.meta.env.VITE_APP_ID,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
+})
+
 initializeFirestore(app, {
   ignoreUndefinedProperties: true
 })
