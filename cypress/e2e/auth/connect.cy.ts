9f2d6aad1ee650aff1b2dc2ef6ac35801473c0a6
Cyril Auquier <cyrilauquier@gmail.com>
2024-10-04 15:41:44 +0200

    fix test

--- cypress/e2e/auth/connect.cy.ts
+++ cypress/e2e/auth/connect.cy.ts
@@ -6,7 +6,7 @@ describe('Connect a user', () => {
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(5000)
-    cy.contains('h1', 'Register')
+    cy.contains('h1', 'Login')
   })
 
   it('connect user', () => {
@@ -14,7 +14,7 @@ describe('Connect a user', () => {
 
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(5000)
-    cy.contains('h1', 'Register')
+    cy.contains('h1', 'Login')
     cy.get('input[name="email"]').type(Cypress.env('test_connect_user_login'))
     cy.get('input[name="password"]').type(Cypress.env('test_connect_user_pwd'))
     cy.get('form button[type=submit]').click()
