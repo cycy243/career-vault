9fad27b9c5bf930eb012975c6bbc721808fce12a
Cyril Auquier <cyrilauquier@gmail.com>
2024-10-06 16:19:13 +0200

    add vue dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import VueDevTools from 'vite-plugin-vue-devtools'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue(),
     visualizer({ open: true, filename: 'bundle-visualization.html' }) as PluginOption
   ],
