66d85d25efe1c7f4909678f62fc1590e3e6b02d0
Cyril Auquier <cyrilauquier@gmail.com>
2024-11-16 09:33:35 +0100

    - update form to have field for spontaneous application - add test to the component

--- src/components/__tests__/form/jobApplicationForm.spec.ts
+++ src/components/__tests__/form/jobApplicationForm.spec.ts
@@ -1,12 +1,11 @@
-import { describe, it, expect, assert } from 'vitest'
+import { describe, it, expect, assert, vi } from 'vitest'
 
-import * as yup from 'yup'
-import { flushPromises, mount, shallowMount } from '@vue/test-utils'
+import { enableAutoUnmount, flushPromises, mount, shallowMount } from '@vue/test-utils'
 import JobApplicationForm from '@/components/form/JobApplicationForm.vue'
-import { nextTick } from 'vue'
-import { toTypedSchema } from '@vee-validate/yup'
-import { useForm } from 'vee-validate'
+import { nextTick, ref } from 'vue'
 import JobApplication from '@/modules/model/jobApplication'
+import { afterEach } from 'node:test'
+import * as applications from '@/components/__tests__/jobApplicationData'
 
 describe('JobApplicationForm test', () => {
   it('if form is empty or not valid then no submit is emmitted when button clicked', () => {
@@ -15,6 +14,7 @@ describe('JobApplicationForm test', () => {
     wrapper.find('form').trigger('submit')
 
     expect(wrapper.emitted()).not.toHaveProperty('submit')
+    wrapper.unmount()
   })
 
   //   it('emits event when submit', async () => {
@@ -64,10 +64,34 @@ describe('JobApplicationForm test', () => {
   //     expect(emits).toHaveProperty('submit')
   //   })
 
-  it("when a 'jobApplication' is gived then use it to complete form", () => {
+  it('when type is "job_offers" and form is submitting without being completed than there is an error for the offerDetails input', async () => {
+    const wrapper = mount(JobApplicationForm)
+
+    wrapper.find("input[value='job_offer']").trigger('click')
+    wrapper.find("input[value='job_offer']").trigger('change')
+    expect(
+      (wrapper.find("input[value='job_offer']").element as HTMLInputElement).checked
+    ).toBeTruthy()
+
+    await nextTick()
+    await flushPromises()
+
+    wrapper.find('button').trigger('submit')
+
+    await nextTick()
+    await flushPromises()
+
+    expect(
+      wrapper.find('div:has(input[name="offerDetails"]) ~ .errors-wrapper').text()
+    ).toBeTruthy()
+  })
+
+  it("when a 'jobApplication' is gived then use it to complete form", async () => {
     const application = new JobApplication(
       'Fake society4',
       'Fake post4',
+      'Fake trus',
+      new Date(Date.now()),
       new Date(Date.now()),
       false,
       undefined,
@@ -75,6 +99,15 @@ describe('JobApplicationForm test', () => {
       '4'
     )
     const wrapper = mount(JobApplicationForm, { props: { jobApplication: application } })
+
+    wrapper.find("input[value='job_offer']").trigger('click')
+    wrapper.find("input[value='job_offer']").trigger('change')
+    expect(
+      (wrapper.find("input[value='job_offer']").element as HTMLInputElement).checked
+    ).toBeTruthy()
+
+    await nextTick()
+    await flushPromises()
 
     expect((wrapper.find('input[name="societyName"]').element as HTMLInputElement).value).toBe(
       application.societyName
@@ -93,5 +126,73 @@ describe('JobApplicationForm test', () => {
     expect((wrapper.find('input[name="offerDetails"]').element as HTMLInputElement).value).toBe(
       application.applicationLink
     )
+  })
+
+  it('when type is "spontaneous" and form is submitting without being completed than there are not error for field like "offerDetails"', async () => {
+    const wrapper = mount(JobApplicationForm)
+
+    expect(
+      (wrapper.find("input[value='job_offer']").element as HTMLInputElement).checked
+    ).toBeFalsy()
+    expect(
+      (wrapper.find("input[value='spontaneous']").element as HTMLInputElement).checked
+    ).toBeTruthy()
+
+    await nextTick()
+    await flushPromises()
+
+    wrapper.find('form').trigger('submit')
+
+    await nextTick()
+    await flushPromises()
+
+    expect(
+      wrapper.find('div:has(input[name="offerDetails"]) ~ .errors-wrapper').exists()
+    ).toBeFalsy()
+  })
+
+  it('complete form for spontaneous application and submit give the application', async () => {
+    const application = applications.spontaneaousApplication
+    const submittedApplication = ref<JobApplication | undefined>()
+    const onSubmitSpy = vi.fn()
+
+    // Mount the component with an 'onSubmit' prop handler
+    const wrapper = mount(JobApplicationForm, { props: { onSubmit: onSubmitSpy } })
+    const emitSpy = vi.spyOn(wrapper.vm, '$emit')
+
+    await nextTick()
+    await flushPromises()
+
+    // Fill out form inputs
+    await wrapper
+      .find('input[name="societyName"]')
+      .setValue(application.companyInformation!.societyName)
+    await wrapper
+      .find('input[name="sendDate"]')
+      .setValue(application.sendDate?.toISOString().slice(0, 10))
+    await wrapper.find('input[name="contactEmail"]').setValue(application.contactInformation?.mail)
+
+    // Click the checkbox and ensure it's checked
+    const isAcceptedCheckbox = wrapper.find("input[name='isAccepted']")
+    await isAcceptedCheckbox.trigger('click')
+    await isAcceptedCheckbox.trigger('change')
+    expect((isAcceptedCheckbox.element as HTMLInputElement).checked).toBeTruthy()
+
+    await flushPromises()
+
+    // Submit the form
+    await wrapper.find('button').trigger('submit')
+    wrapper.vm.$forceUpdate()
+    await nextTick() // Make sure Vue has finished any async operations
+    await flushPromises()
+
+    // Debugging outputs to check state
+    const formApplication = wrapper.emitted()
+    console.log('Form emitted events:', formApplication)
+    console.log('Submitted application:', (formApplication['submit']?.[0] as unknown[])?.[0])
+
+    // Assert that the application was received by the onSubmit callback
+    expect(onSubmitSpy).toHaveBeenCalled()
+    expect(submittedApplication.value).toBeDefined()
   })
 })
--- src/components/form/JobApplicationForm.vue
+++ src/components/form/JobApplicationForm.vue
@@ -1,6 +1,18 @@
 <template>
   <form @submit="onSubmit">
     <fieldset>
+      <FormGroupInputRadio
+        label="Application type"
+        :options="[
+          { inputValue: 'spontaneous', value: 'Spatoneaous application' },
+          { inputValue: 'job_offer', value: 'Job offer' }
+        ]"
+        input_name="application_type"
+        v-model:value="applicationType"
+        v-bind="applicationTypeAttrs"
+      />
+    </fieldset>
+    <fieldset>
       <legend>General</legend>
       <FormInput
         :name="'societyName'"
@@ -12,13 +24,54 @@
         v-bind="societyNameAttrs"
       />
       <FormInput
-        :name="'jobTitle'"
-        title="Job's title"
-        :error="errors.jobTitle"
-        :defaultValue="jobApplication?.jobTitle"
+        :name="'societyWebSite'"
+        :title="'Company website'"
+        :error="errors.societyWebsite"
         :type="'text'"
-        v-model="jobTitle"
-        v-bind="jobTitleAttrs"
+        :defaultValue="jobApplication?.societyWebsite"
+        v-model="societyWebsite"
+        v-bind="societyWebsiteAttrs"
+      />
+      <template v-if="applicationType === 'job_offer'">
+        <FormInput
+          :name="'jobTitle'"
+          title="Job's title"
+          :error="errors.jobTitle"
+          :defaultValue="jobApplication?.jobTitle"
+          :type="'text'"
+          v-model="jobTitle"
+          v-bind="jobTitleAttrs"
+        />
+      </template>
+    </fieldset>
+    <fieldset>
+      <legend>Contact</legend>
+      <FormInput
+        :name="'contactName'"
+        :title="'Name'"
+        :error="errors.contactName"
+        :type="'text'"
+        :defaultValue="jobApplication?.contactInformation?.name"
+        v-model="contactName"
+        v-bind="contactNameAttrs"
+      />
+      <FormInput
+        :name="'contactEmail'"
+        :title="'Email'"
+        :error="errors.contactEmail"
+        :type="'email'"
+        :defaultValue="jobApplication?.contactInformation?.mail"
+        v-model="contactEmail"
+        v-bind="contactEmailAttrs"
+      />
+      <FormInput
+        :name="'contactFunction'"
+        :title="'Function'"
+        :error="errors.contactFunction"
+        :type="'text'"
+        :defaultValue="jobApplication?.contactInformation?.function"
+        v-model="contactFunction"
+        v-bind="contactFunctionAttrs"
       />
     </fieldset>
     <fieldset>
@@ -34,12 +87,21 @@
       />
       <FormInput
         name="responseDate"
-        title="Date response"
+        title="Date refusing"
         :error="errors.responseDate"
         :defaultValue="jobApplication?.responseDate?.toISOString().slice(0, 10)"
         type="date"
         v-model="responseDate"
         v-bind="responseDateAttrs"
+      />
+      <FormInput
+        name="interviewDate"
+        title="Date interview"
+        :error="errors.interviewDate"
+        :defaultValue="jobApplication?.interviewDate?.toISOString().slice(0, 10)"
+        type="date"
+        v-model="interviewDate"
+        v-bind="interviewDateAttrs"
       />
       <FormInput
         name="isAccepted"
@@ -51,7 +113,7 @@
         v-bind="isAcceptedAttrs"
       />
     </fieldset>
-    <fieldset>
+    <fieldset v-if="applicationType === 'job_offer'">
       <legend>Details</legend>
       <label for="offerDetails">Offer's details</label>
       <input
@@ -60,18 +122,32 @@
         v-bind="offerDetailsAttrs"
         @change.prevent="offerDetailsFileChanged($event)"
       />
-      <div>{{ errors.offerDetails }}</div>
       <FormInput
         name="offerDetails"
         title="Offer's details"
-        :error="errors.offerDetails"
         :defaultValue="jobApplication?.applicationLink"
         type="text"
         v-model="offerDetails"
         v-bind="offerDetailsAttrs"
         @update:modelValue="(value) => offerDetailsChange(value)"
       />
+      <div class="errors-wrapper">{{ errors.offerDetails }}</div>
     </fieldset>
+    <!-- To decomment later because this is for adding "proof" -->
+    <!-- <fieldset>
+      <legend>Document</legend>
+      <p>Document like resume or cover letter send to the employer</p>
+      <input
+        name="offerDetails"
+        type="file"
+        v-bind="offerDetailsAttrs"
+        @change.prevent="onNewDocumentAdded($event)"
+      />
+      <p v-if="otherFiles.length === 0">No file added</p>
+      <ul v-else>
+        <li v-for="(file, index) in otherFiles" :key="index">{{ file.name }}</li>
+      </ul>
+    </fieldset> -->
     <button type="submit">Add</button>
   </form>
 </template>
@@ -82,6 +158,7 @@ import * as yup from 'yup'
 import FormInput from './FormInput.vue'
 import JobApplication from '@/modules/model/jobApplication'
 import { ref, watch } from 'vue'
+import FormGroupInputRadio from './FormGroupInputRadio.vue'
 
 type JobApplicationFormProps = {
   jobApplication?: JobApplication
@@ -109,37 +186,76 @@ watch(
 
 const schema = toTypedSchema(
   yup.object({
-    societyName: yup.string().required(),
-    jobTitle: yup.string().required(),
-    sendDate: yup.date(),
+    societyName: yup.string().required("The company's name is required"),
+    contactName: yup.string(),
+    contactEmail: yup
+      .string()
+      .email('The email should be a valid email')
+      .when('applicationType', {
+        is: (applicationType: string) => isSpontaneousApplication(applicationType),
+        then: () =>
+          yup
+            .string()
+            .email('The email should be a valid email')
+            .required('You should provide the email you use to submit your application')
+      }),
+    contactFunction: yup.string(),
+    societyWebsite: yup.string().url("The website's url should be a valid url"),
+    jobTitle: yup.string().when('applicationType', {
+      is: (applicationType: string) => !isSpontaneousApplication(applicationType),
+      then: () => yup.string().required("The job's title is required")
+    }),
+    sendDate: yup.date().required('The sending date is required'),
+    interviewDate: yup.date(),
     responseDate: yup.date(),
-    isAccepted: yup.boolean().required(),
-    offerDetails: yup.mixed().required()
+    isAccepted: yup.boolean().required('The accepting status is required'),
+    applicationType: yup.string().required('You should provide what is your application is about'),
+    offerDetails: yup.mixed().when('applicationType', {
+      is: (applicationType: string) => !isSpontaneousApplication(applicationType),
+      then: () => yup.mixed().required("You have to provide the offer's details")
+    })
   })
 )
 
+const isSpontaneousApplication = (applicationType: string) => applicationType !== 'job_offer'
+
 const { defineField, handleSubmit, errors, resetForm } = useForm({
+  initialValues: {
+    applicationType: 'spontaneous'
+  },
   validationSchema: schema
 })
 
 type JobApplicationFormEmits = {
   (e: 'submit', value: JobApplication, application: File | string): void
+  (e: 'new-application', value: JobApplication, application: File | string): void
 }
 const applicationFile = ref<File | string>()
+const otherFiles = ref<Array<File>>([])
 
 const emit = defineEmits<JobApplicationFormEmits>()
 
-const onSubmit = handleSubmit((values) => {
-  const submittedApplication = new JobApplication(
-    values.societyName,
-    values.jobTitle,
-    values.sendDate,
-    values.isAccepted,
-    values.responseDate
-  )
-  submittedApplication.applicationId = props.jobApplication?.applicationId
-  emit('submit', submittedApplication, applicationFile.value || '')
-})
+const onSubmit = handleSubmit(
+  (values) => {
+    const submittedApplication = new JobApplication(
+      values.societyName,
+      values.societyWebsite,
+      values.jobTitle,
+      values.sendDate,
+      values.interviewDate,
+      values.isAccepted,
+      values.responseDate
+    )
+
+    submittedApplication.applicationId = props.jobApplication?.applicationId
+    console.log('chch')
+
+    emit('submit', submittedApplication, applicationFile.value || '')
+  },
+  ({ errors }) => {
+    console.log(errors)
+  }
+)
 
 function offerDetailsFileChanged($event: Event) {
   // eslint-disable-next-line no-unsafe-optional-chaining
@@ -149,6 +265,15 @@ function offerDetailsFileChanged($event: Event) {
     : [...((event.target as any)?.files as FileList)]
   offerDetails.value = files[0]
   offerDetailsChange(files[0])
+}
+
+function onNewDocumentAdded($event: Event) {
+  // eslint-disable-next-line no-unsafe-optional-chaining
+  const event = $event as InputEvent
+  const files = event.dataTransfer
+    ? [...event.dataTransfer.files]
+    : [...((event.target as any)?.files as FileList)]
+  otherFiles.value = [...otherFiles.value, ...files]
 }
 
 function offerDetailsChange(offerDetails: File | string) {
@@ -162,5 +287,11 @@ const [isAccepted, isAcceptedAttrs] = defineField('isAccepted', validationOption
 const [sendDate, sendDateAttrs] = defineField('sendDate', validationOptions)
 const [responseDate, responseDateAttrs] = defineField('responseDate', validationOptions)
 const [offerDetails, offerDetailsAttrs] = defineField('offerDetails', validationOptions)
+const [applicationType, applicationTypeAttrs] = defineField('applicationType', validationOptions)
+const [interviewDate, interviewDateAttrs] = defineField('interviewDate', validationOptions)
+const [societyWebsite, societyWebsiteAttrs] = defineField('societyWebsite', validationOptions)
+const [contactEmail, contactEmailAttrs] = defineField('contactEmail', validationOptions)
+const [contactFunction, contactFunctionAttrs] = defineField('contactFunction', validationOptions)
+const [contactName, contactNameAttrs] = defineField('contactName', validationOptions)
 </script>
 <style lang="css"></style>
